6a246aa8ffd2ce711d8a0caf31031a01c12b53ea
Hue <skeptikal.monke@gmail.com>
2026-02-13 18:45:37 +0100

    basic client/cli

---
+++ src/client/main.c
@@ -0,0 +1,102 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <limits.h>
+#include <sys/un.h>
+#include <sys/socket.h>
+
+#include "../ipc-codes.h"
+
+int main(int argc, char *argv[]){
+	char *path = "/tmp/tstbd.sock";
+
+	#define BUF_SIZE 100
+	char buf[BUF_SIZE];
+	#undef BUF_SIZE
+	int pos = 0;
+
+	if (strcmp(argv[1], "send-all") == 0){
+		if (argc < 3){
+			fprintf(stderr, "Usage: send-all <signal>\n");
+			return 1;
+		}
+		buf[pos++] = IPC_SEND_ALL;
+		buf[pos++] = atoi(argv[2]);
+	}
+	else if (strcmp(argv[1], "send-by-pid") == 0){
+		if (argc < 4){
+			fprintf(stderr, "Usage: send-by-pid <pid> <signal>\n");
+			return 1;
+		}
+		buf[pos++] = IPC_SEND_PID;
+
+		unsigned pid = atoi(argv[2]);
+		for (int a = 0; a < sizeof(int); a++){
+		buf[pos++] = (pid >> (a * CHAR_BIT));
+		}
+		buf[pos++] = atoi(argv[3]);
+	}
+	else if (strcmp(argv[1], "send-by-nam") == 0){
+		if (argc < 4){
+			fprintf(stderr, "Usage: send-by-nam <name> <signal>\n");
+			return 1;
+		}
+		buf[pos++] = IPC_SEND_NAM;
+
+		size_t name_len = strlen(argv[2]) + 1;
+		memcpy(buf + pos, argv[2], name_len);
+		pos += name_len;
+		buf[pos++] = atoi(argv[3]);
+	}
+	else if (strcmp(argv[1], "reg-proc") == 0){
+		if (argc < 3){
+			fprintf(stderr, "Usage: reg-proc <pid>\n");
+			return 1;
+		}
+		buf[pos++] = IPC_REG_PROC;
+
+		unsigned pid = atoi(argv[2]);
+		for (int a = 0; a < sizeof(int); a++){
+			buf[pos++] = (pid >> (a * CHAR_BIT));
+		}
+	}
+	else if (strcmp(argv[1], "reg-rule") == 0){
+		if (argc < 4){
+			fprintf(stderr, "Usage: reg-rule <target> <action>\n");
+			return 1;
+		}
+		buf[pos++] = IPC_REG_RULE;
+
+		size_t target_len = strlen(argv[2]) + 1;
+		memcpy(buf + pos, argv[2], target_len);
+		pos += target_len;
+
+		size_t action_len = strlen(argv[3]) + 1;
+		memcpy(buf + pos, argv[3], action_len);
+		pos += action_len;
+	}
+	else {
+		fprintf(stderr, "Unknown command: %s\n", argv[1]);
+		return 1;
+	}
+
+	int sock = socket(AF_UNIX, SOCK_STREAM, 0);
+	if (sock == -1)
+		return 1;
+
+	struct sockaddr_un addr;
+	memset(&addr, 0, sizeof(struct sockaddr_un));
+	addr.sun_family = AF_UNIX;
+	strncpy(addr.sun_path, path, sizeof(addr.sun_path) -1);
+
+	if (connect(sock, (struct sockaddr*)&addr, sizeof(struct sockaddr_un)) == -1)
+		return 2;
+
+	write(sock, buf, pos);
+	close(sock);
+
+	exit(EXIT_SUCCESS);
+
+	return 0;
+}
